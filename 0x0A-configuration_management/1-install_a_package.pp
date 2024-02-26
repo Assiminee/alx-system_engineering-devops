96c1064d1ff44e3463a9f62bd9ce2b7714530dbb
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-02-26 22:36:39 +0100

    test2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # installs flask from pip3 using Puppet
 
-
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
