ddc35caf8b7c039a5f4ccabee9102a30f93d4cf1
Assiminee <znatni.yasmine@gmail.com>
2024-07-11 23:21:45 +0100

    msg

--- 0x10-https_ssl/0-world_wide_web.sh
+++ 0x10-https_ssl/0-world_wide_web.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Displays information about subdomains
 
-subdomains=("www" "lb-01" "web-01" "web-02")
 subdomain_info() {
     dig "$1" | grep -A1 'ANSWER SECTION:' | sed -r "s/$1./$2/g" | awk 'NR==2 {print "The subdomain " $1 " is a " $4 " record and points to " $5}' 
 }
@@ -15,8 +14,4 @@ then
     subdomain_info "lb-01.$1" "lb-01"
     subdomain_info "web-01.$1" "web-01"
     subdomain_info "web-02.$1" "web-02"
-    #for subdomain in "${subdomains[@]}";
-    #do
-    #    subdomain_info "$subdomain.$1" "$subdomain"
-    #done
 fi
