ec9d55b6c3a453fcc6804ad510fdc253cc26035c
Assiminee <znatni.yasmine@gmail.com>
2024-07-29 16:38:05 +0100

    msg

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+communicates with jsonplaceholder api to get
+todos of employee with <userId> (passed as
+command line argument) and displays the info
+"""
+
+import requests
+import sys
+import json
+
+
+def display_info():
+    """
+    Displays employee todos
+    """
+    if len(sys.argv) < 2:
+        return
+
+    userId = sys.argv[1]
+
+    todoURL = f"https://jsonplaceholder.typicode.com/todos?userId={userId}"
+    userURL = f"https://jsonplaceholder.typicode.com/users/{userId}"
+
+    todoResp = requests.get(todoURL).json()
+    userResp = requests.get(userURL).json()
+
+    username = userResp.get('username')
+
+    jsonData = {f"{userId}": []}
+
+    for todo in todoResp:
+        keys = ['task', 'completed', 'username']
+        todo['task'] = todo.pop('title')
+        todo['username'] = username
+
+        jsonData[f"{userId}"].append({key: todo[key] for key in keys})
+
+    with open(f"{userId}.json", "w") as jsonFile:
+        json.dump(jsonData, jsonFile)
+
+
+if __name__ == "__main__":
+    display_info()
