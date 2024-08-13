e5d402301806a3445af17b14eb1110845ed36609
Assiminee <znatni.yasmine@gmail.com>
2024-08-13 20:31:36 +0100

    msg

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Subreddit sub-count
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the sub-count of a subreddit
+    using Reddit's API
+    """
+    user_agent = 'assiminee'
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+
+    headers = {'User-Agent': user_agent}
+
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+
+    if resp.status_code != 200:
+        return 0
+    
+    data = resp.json()['data']
+    location = data['children'][0]['data']
+    return location['subreddit_subscribers'], 'OK'
